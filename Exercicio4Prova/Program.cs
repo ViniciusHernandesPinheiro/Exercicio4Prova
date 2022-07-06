e476d5a7a54ea3df3b5a88298c05079bb5d6f66e
vinic <vinic@PC-VINI>
2022-07-06 16:39:06 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio4Prova/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio4Prova
+{
+    internal class Program
+    {
+        //RA: 6322036
+        //Nome: Vinícius Hernandes Pinheiro
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercicio 4");
+
+            Console.WriteLine("Numeros de 1 até 50");
+
+            for (int i = 1; i <= 50; i++)
+            {
+                Console.WriteLine(i);                  
+            }
+            
+        }
+    }
+}
